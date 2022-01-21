40196968c8a470c9899ec98974878aeb7cd31d9d
alexmbennett2 <alexmbennett2@gmail.com>
2022-01-21 15:05:12 -0500

    Updated micro_test query structure

--- sql/fhir_observation_micro_test.sql
+++ sql/fhir_observation_micro_test.sql
@@ -1,82 +1,87 @@
 DROP TABLE IF EXISTS mimic_fhir.observation_micro_test;
 CREATE TABLE mimic_fhir.observation_micro_test(
-	id 		uuid PRIMARY KEY,
-  	fhir 	jsonb NOT NULL 
+    id      uuid PRIMARY KEY,
+    fhir    jsonb NOT NULL 
 );
 
+
+-- Group to avoid duplicate organisms showing up for a given subject's test
 WITH distinct_org AS (
 	SELECT DISTINCT
 		mi.micro_specimen_id  AS mi_MICRO_SPECIMEN_ID
         , CAST(mi.test_itemid AS TEXT) AS mi_TEST_ITEMID
-        , mi.test_name AS mi_TEST_NAME
-        , mi.subject_id AS mi_SUBJECT_ID
-        , mi.hadm_id AS mi_HADM_ID
-        , CAST(mi.charttime AS TIMESTAMPTZ) AS mi_CHARTTIME
-
-        -- UUID references
-        , uuid_generate_v5(ns_observation_micro_test.uuid, mi.micro_specimen_id || '-' || mi.test_itemid) AS uuid_MICRO_TEST
-        , uuid_generate_v5(ns_patient.uuid, CAST(mi.subject_id AS TEXT)) AS uuid_SUBJECT_ID
-        , uuid_generate_v5(ns_encounter.uuid, CAST(mi.hadm_id AS TEXT)) AS uuid_HADM_ID	
+        , MAX(mi.test_name) AS mi_TEST_NAME
+        , MAX(mi.subject_id) AS mi_SUBJECT_ID
+        , MAX(mi.hadm_id) AS mi_HADM_ID
+        , MAX(CAST(mi.charttime AS TIMESTAMPTZ)) AS mi_CHARTTIME
 		
 		, CASE WHEN MIN(mi.org_itemid) IS NULL THEN NULL 
 		  ELSE
-	        jsonb_build_object('reference', 
-	                        'Observation/' || uuid_generate_v5(ns_observation_micro_org.uuid, mi.micro_specimen_id || '-' || mi.org_itemid)
-	          ) 
-	      END as fhir_ORGANISMS
+	          mi.test_itemid || '-' || mi.micro_specimen_id || '-' || mi.org_itemid
+	      END as mi_ORGANISM
+	    
+	    -- flag for whether a specimen has at least one organism growth  
 	    , CASE WHEN MIN(mi.org_itemid) IS NULL THEN FALSE ELSE TRUE END AS valueBoolean
 	FROM 
-	    mimic_hosp.microbiologyevents mi
-	   	INNER JOIN fhir_etl.subjects sub
-	   		ON mi.subject_id = sub.subject_id 
-	   	LEFT JOIN fhir_etl.uuid_namespace ns_patient
-  			ON ns_patient.name = 'Patient'
-  			LEFT JOIN fhir_etl.uuid_namespace ns_encounter
-  			ON ns_encounter.name = 'Encounter'
-  		LEFT JOIN fhir_etl.uuid_namespace ns_observation_micro_test
-  			ON ns_observation_micro_test.name = 'ObservationMicroTest'
-  		LEFT JOIN fhir_etl.uuid_namespace ns_observation_micro_org
-  			ON ns_observation_micro_org.name = 'ObservationMicroOrg'
+        mimic_hosp.microbiologyevents mi
+        INNER JOIN fhir_etl.subjects sub
+            ON mi.subject_id = sub.subject_id 
 	GROUP BY 
 	     test_itemid
-        , test_name
         , micro_specimen_id
-        , mi.subject_id
         , org_itemid
-        , charttime
-        , hadm_id
-        , ns_patient.uuid
-        , ns_encounter.uuid
-        , ns_observation_micro_test.uuid
-        , ns_observation_micro_org.uuid
-), fhir_observation_micro_test AS (
+), grouped_org AS (
 	SELECT 
 		mi_MICRO_SPECIMEN_ID
 	    , mi_TEST_ITEMID
-	    , mi_TEST_NAME
-	    , mi_SUBJECT_ID
-	    , mi_HADM_ID
-	    , mi_CHARTTIME
-	    , uuid_MICRO_TEST
-	    , uuid_SUBJECT_ID
-	    , uuid_HADM_ID
-	    , valueBoolean
-		, CASE WHEN valueBoolean THEN json_agg(fhir_ORGANISMS) 
-		  ELSE NULL END AS fhir_ORGANISMS
+	    , MAX(mi_TEST_NAME) AS mi_TEST_NAME
+	    , MAX(mi_SUBJECT_ID) AS mi_SUBJECT_ID
+	    , MAX(mi_HADM_ID) AS mi_HADM_ID
+	    , MAX(mi_CHARTTIME) AS mi_CHARTTIME
+	    , BOOL_OR(valueBoolean) AS valueBoolean  
+
+	    -- only include organism list if specimen had at least one organism growth
+		, CASE WHEN BOOL_OR(valueBoolean) THEN 
+		    json_agg(
+		        jsonb_build_object('reference', 
+	                        'Observation/' || uuid_generate_v5(ns_observation_micro_org.uuid, mi_ORGANISM)
+	            ) 
+	        )            
+		  ELSE NULL END AS fhir_ORGANISM
 	FROM 
 		distinct_org
+		LEFT JOIN fhir_etl.uuid_namespace ns_observation_micro_org
+  		    ON ns_observation_micro_org.name = 'ObservationMicroOrg'
 	GROUP BY 
 	    mi_MICRO_SPECIMEN_ID
 	    , mi_TEST_ITEMID
-	    , mi_TEST_NAME
-	    , mi_SUBJECT_ID
-	    , mi_HADM_ID
-	    , mi_CHARTTIME
-	    , uuid_MICRO_TEST
-	    , uuid_SUBJECT_ID
-	    , uuid_HADM_ID
-	    , valueBoolean
+	    , ns_observation_micro_org.uuid
+
+), fhir_observation_micro_test AS (
+    SELECT 
+        mi_MICRO_SPECIMEN_ID
+        , mi_TEST_ITEMID
+        , mi_TEST_NAME
+        , mi_SUBJECT_ID
+        , mi_HADM_ID
+        , mi_CHARTTIME
+        , valueBoolean
+        , fhir_ORGANISM
+        
+        -- UUID references
+        , uuid_generate_v5(ns_observation_micro_test.uuid, mi_MICRO_SPECIMEN_ID|| '-' || mi_TEST_ITEMID) AS uuid_MICRO_TEST
+        , uuid_generate_v5(ns_patient.uuid, CAST(mi_SUBJECT_ID AS TEXT)) AS uuid_SUBJECT_ID
+        , uuid_generate_v5(ns_encounter.uuid, CAST(mi_HADM_ID AS TEXT)) AS uuid_HADM_ID	
+    FROM
+        grouped_org
+        LEFT JOIN fhir_etl.uuid_namespace ns_patient
+            ON ns_patient.name = 'Patient'
+        LEFT JOIN fhir_etl.uuid_namespace ns_encounter
+            ON ns_encounter.name = 'Encounter'
+        LEFT JOIN fhir_etl.uuid_namespace ns_observation_micro_test
+            ON ns_observation_micro_test.name = 'ObservationMicroTest'
 )
+
 INSERT INTO mimic_fhir.observation_micro_test  
 SELECT 
 	  uuid_MICRO_TEST AS id
@@ -104,8 +109,9 @@ SELECT
       		ELSE NULL
       		END
         , 'effectiveDateTime', mi_CHARTTIME
-        , 'hasMember', fhir_ORGANISMS
+        , 'hasMember', fhir_ORGANISM
       	, 'valueBoolean', valueBoolean
     )) AS fhir 
 FROM
     fhir_observation_micro_test
+    
