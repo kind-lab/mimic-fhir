92f17115d5b4ff66ae558c786d5509b3d67370b9
alexmbennett2 <alexmbennett2@gmail.com>
2022-03-09 16:48:40 -0500

    Added Bundler and updated tests for bundling

--- py_mimic_fhir/bundle.py
+++ py_mimic_fhir/bundle.py
@@ -4,6 +4,11 @@
 import logging
 import requests
 import json
+from pathlib import Path
+import os
+import pandas as pd
+
+from py_mimic_fhir import db
 
 
 class Bundle():
@@ -11,7 +16,6 @@ class Bundle():
         self.resourceType = 'Bundle'
         self.type = 'transaction'
         self.entry = []
-        self.response = ''
 
     def add_entry(self, resources):
         for resource in resources:
@@ -39,4 +43,132 @@ class Bundle():
         output = json.loads(resp.text)
         if output['resourceType'] == 'OperationOutcome':
             logging.error(output)
-        self.response = output
+        return output
+
+
+# Generic function to get resources linked to patient from the DB
+def get_resources_by_pat(db_conn, table_name, patient_id):
+    q_resource = f"""
+        SELECT fhir FROM mimic_fhir.{table_name}
+        WHERE patient_id = '{patient_id}'
+    """
+    pd_resources = pd.read_sql_query(q_resource, db_conn)
+    resources = pd_resources.fhir.to_list()
+
+    return resources
+
+
+# Generic function to get single resource from the DB
+def get_patient_resource(db_conn, patient_id):
+    q_resource = f"SELECT * FROM mimic_fhir.patient WHERE id='{patient_id}'"
+    resource = pd.read_sql_query(q_resource, db_conn)
+
+    return resource.fhir[0]
+
+
+# Class to bundle all resources associated with one patient
+class Bundler():
+    def __init__(self, patient_id):
+        self.patient_id = patient_id
+        self.patient_bundle = Bundle()
+        self.micro_bundle = Bundle()
+        self.med_bundle = Bundle()
+        self.lab_bundle = Bundle()
+        self.icu_base_bundle = Bundle()
+        self.icu_obs_bundle = Bundle()
+        self.db_conn = db.db_conn()
+
+    def generate_all_bundles(self):
+        self.generate_patient_bundle()
+        self.generate_micro_bundle()
+        #self.generate_med_bundle() # Activate when medication PR is integrated
+        self.generate_lab_bundle()
+        self.generate_icu_base_bundle()
+        self.generate_icu_obs_bundle()
+
+    def generate_patient_bundle(self):
+        table_list = ['encounter', 'condition', 'procedure']
+
+        # Add individual patient
+        pat_resource = get_patient_resource(self.db_conn, self.patient_id)
+        self.patient_bundle.add_entry([pat_resource])
+
+        # Add all base patient resources for the Patient to the bundle
+        for table_name in table_list:
+            resources = get_resources_by_pat(
+                self.db_conn, table_name, self.patient_id
+            )
+            self.patient_bundle.add_entry(resources)
+
+    def generate_micro_bundle(self):
+        table_list = [
+            'observation_micro_test', 'observation_micro_org',
+            'observation_micro_susc'
+        ]
+
+        # Add all micro resources associated with the Patient to the bundle
+        for table_name in table_list:
+            resources = get_resources_by_pat(
+                self.db_conn, table_name, self.patient_id
+            )
+            self.micro_bundle.add_entry(resources)
+
+    def generate_med_bundle(self):
+        table_list = [
+            'medication_request', 'medication_dispense',
+            'medication_administration'
+        ]
+
+        # Add all medication resources associated with the Patient to the bundle
+        for table_name in table_list:
+            resources = get_resources_by_pat(
+                self.db_conn, table_name, self.patient_id
+            )
+            self.med_bundle.add_entry(resources)
+
+    def generate_lab_bundle(self):
+        table_list = ['observation_labs']
+
+        # Add all lab resources associated with the Patient to the bundle
+        for table_name in table_list:
+            resources = get_resources_by_pat(
+                self.db_conn, table_name, self.patient_id
+            )
+            self.lab_bundle.add_entry(resources)
+
+    def generate_icu_base_bundle(self):
+        table_list = [
+            'encounter_icu', 'procedure_icu', 'medication_administration_icu'
+        ]
+
+        # Add all ICU base resources associated with the Patient to the bundle
+        for table_name in table_list:
+            resources = get_resources_by_pat(
+                self.db_conn, table_name, self.patient_id
+            )
+            self.icu_base_bundle.add_entry(resources)
+
+    def generate_icu_obs_bundle(self):
+        table_list = [
+            'observation_chartevents', 'observation_datetimeevents',
+            'observation_outputevents'
+        ]
+
+        # Add all ICU observation resources associated with the Patient to the bundle
+        for table_name in table_list:
+            resources = get_resources_by_pat(
+                self.db_conn, table_name, self.patient_id
+            )
+            self.icu_obs_bundle.add_entry(resources)
+
+    def post_all_bundles(self, fhir_server):
+        response_list = []
+
+        response_list.append(self.patient_bundle.request(fhir_server))
+        response_list.append(self.micro_bundle.request(fhir_server))
+        #self.med_bundle.request(fhir_server)
+        response_list.append(self.lab_bundle.request(fhir_server))
+        response_list.append(self.icu_base_bundle.request(fhir_server))
+        response_list.append(self.icu_obs_bundle.request(fhir_server))
+
+        return response_list
--- tests/test_bundles.py
+++ tests/test_bundles.py
@@ -18,11 +18,73 @@ import os
 import subprocess
 import pytest
 import numpy as np
+import pandas as pd
 
 #from fhir.resources.bundle import Bundle
-from py_mimic_fhir.bundle import Bundle
+from py_mimic_fhir.bundle import Bundle, Bundler
 
 FHIR_SERVER = os.getenv('FHIR_SERVER')
+
+
+# Function to find links between a patient and certain resources.
+# Allows for more complete testing if sending full bundle
+def get_pat_id_with_links(db_conn, resource_list):
+    q_resource = 'SELECT pat.fhir FROM mimic_fhir.patient pat'
+
+    # Dynamically create query to find links between a patient and certain resources
+    for idx, resource in enumerate(resource_list):
+        q_resource = f"""{q_resource} 
+            INNER JOIN mimic_fhir.{resource} t{idx}
+                ON pat.id = t{idx}.patient_id 
+        """
+    q_resource = f'{q_resource} LIMIT 20;'
+
+    resource = pd.read_sql_query(q_resource, db_conn)
+    return resource.fhir[0]['id']
+
+
+# Get n patient_ids
+def get_n_patient_id(db_conn, n_patient):
+    q_resource = f"SELECT * FROM mimic_fhir.patient LIMIT {n_patient}"
+    resource = pd.read_sql_query(q_resource, db_conn)
+    patient_ids = [fhir['id'] for fhir in resource.fhir]
+
+    return patient_ids
+
+
+def test_bundle_patient_all_resources(db_conn):
+    # Get n patient ids to then bundle and post
+    patient_ids = get_n_patient_id(db_conn, 1)
+
+    # Create bundle and post it
+    bundler = Bundler(patient_id)
+    bundler.generate_all_bundles()
+    response_list = bundler.post_all_bundles(FHIR_SERVER)
+
+    result = True
+    for response in response_list:
+        if response['resourceType'] == 'OperationOutcome':
+            result = False
+            logging.error(response)
+    assert result
+
+
+def test_bundle_multiple_patient_all_resources(db_conn):
+    # Get n patient ids to then bundle and post
+    patient_ids = get_n_patient_id(db_conn, 50)
+
+    # Create bundle and post it
+    result = True
+    for patient_id in patient_ids:
+        bundler = Bundler(patient_id)
+        bundler.generate_all_bundles()
+        response_list = bundler.post_all_bundles(FHIR_SERVER)
+
+        for response in response_list:
+            if response['resourceType'] == 'OperationOutcome':
+                result = False
+                logging.error(response)
+    assert result
 
 
 def test_bad_bundle():
@@ -46,51 +108,97 @@ def test_bad_code_in_bundle():
     assert bundle.response['resourceType'] == 'OperationOutcome'
 
 
-def test_patient_bundle(patient_bundle_resources):
-    bundle = Bundle()
-    bundle.add_entry(patient_bundle_resources)
-    bundle.request(FHIR_SERVER)
-    assert bundle.response['resourceType'] == 'Bundle'
+def test_patient_bundle(db_conn):
+    # Get patient_id that has resources from the resource_list
+    resource_list = ['encounter', 'condition', 'procedure']
+    patient_id = get_pat_id_with_links(db_conn, resource_list)
+
+    # Create bundle and post it
+    bundler = Bundler(patient_id)
+    bundler.generate_patient_bundle()
+    response = bundler.patient_bundle.request(FHIR_SERVER)
+    assert response['resourceType'] == 'Bundle'
 
 
-def test_microbio_bundle(microbio_bundle_resources):
-    bundle = Bundle()
-    bundle.add_entry(microbio_bundle_resources)
-    bundle.request(FHIR_SERVER)
-    assert bundle.response['resourceType'] == 'Bundle'
+def test_microbio_bundle(db_conn):
+    # Get patient_id that has resources from the resource_list
+    resource_list = [
+        'observation_micro_test', 'observation_micro_org',
+        'observation_micro_susc'
+    ]
+    patient_id = get_pat_id_with_links(db_conn, resource_list)
+
+    # Create bundle
+    bundler = Bundler(patient_id)
+
+    # Generate and post patient bundle, must do first to avoid referencing issues
+    bundler.generate_patient_bundle()
+    bundler.patient_bundle.request(FHIR_SERVER)
+
+    #  Generate and post micro bundle
+    bundler.generate_micro_bundle()
+    response = bundler.micro_bundle.request(FHIR_SERVER)
+    logging.error(patient_id)
+    assert response['resourceType'] == 'Bundle'
 
 
-def test_lab_bundle(lab_bundle_resources):
-    bundle = Bundle()
-    bundle.add_entry(lab_bundle_resources)
-    bundle.request(FHIR_SERVER)
-    assert bundle.response['resourceType'] == 'Bundle'
+def test_lab_bundle(db_conn):
+    # Get patient_id that has resources from the resource_list
+    resource_list = ['observation_labs']
+    patient_id = get_pat_id_with_links(db_conn, resource_list)
+
+    # Create bundle
+    bundler = Bundler(patient_id)
+
+    # Generate and post patient bundle, must do first to avoid referencing issues
+    bundler.generate_patient_bundle()
+    bundler.patient_bundle.request(FHIR_SERVER)
+
+    #  Generate and post lab bundle
+    bundler.generate_lab_bundle()
+    response = bundler.lab_bundle.request(FHIR_SERVER)
+    logging.error(patient_id)
+    assert response['resourceType'] == 'Bundle'
 
 
-# Only passing a small portion of the meds here (~100)
-def test_med_data_bundle(med_data_bundle_resources):
-    bundle = Bundle()
-    bundle.add_entry(med_data_bundle_resources)
-    bundle.request(FHIR_SERVER)
-    assert bundle.response['resourceType'] == 'Bundle'
+def test_med_pat_bundle(db_conn):
+    # Get patient_id that has resources from the resource_list
+    resource_list = [
+        'medication_request', 'medication_administration'
+    ]  # Add medication_dispense after updating IG
+    patient_id = get_pat_id_with_links(db_conn, resource_list)
+
+    # Create bundle
+    bundler = Bundler(patient_id)
+
+    # Generate and post patient bundle, must do first to avoid referencing issues
+    bundler.generate_patient_bundle()
+    bundler.patient_bundle.request(FHIR_SERVER)
+
+    #  Generate and post micro bundle
+    bundler.generate_med_bundle()
+    response = bundler.med_bundle.request(FHIR_SERVER)
+    logging.error(patient_id)
+    assert response['resourceType'] == 'Bundle'
 
 
 # Only passing a small portion of the meds here (~100)
 def test_iterative_med_data_bundle(med_data_bundle_resources):
     response_list = []
-    resource_splits = np.array_split(med_data_bundle_resources, 5)
+    split_count = len(med_data_bundle_resources) // 500 + 1
+    resource_splits = np.array_split(med_data_bundle_resources, split_count)
     for resource_list in resource_splits:
         bundle = Bundle()
         bundle.add_entry(resource_list)
-        bundle.request(FHIR_SERVER)
-        response_list.append(bundle.response['resourceType'])
-        logging.error(bundle.response)
+        response = bundle.request(FHIR_SERVER)
+        response_list.append(response['resourceType'])
+        logging.error(response)
     assert 'OperationOutcome' not in response_list
 
 
 # Need to figure out how to get medication references working if only a subuset of the Medication resources have been posted
 # OR just post all medication resources..
-def test_med_pat_bundle(med_pat_bundle_resources):
+def test_med_data_bundle(db_conn):
     bundle = Bundle()
     bundle.add_entry(med_pat_bundle_resources)
     bundle.request(FHIR_SERVER)
@@ -98,16 +206,45 @@ def test_med_pat_bundle(med_pat_bundle_resources):
 
 
 # Test icu base bundle that include EncounterICU and MedicationAdminstrationICU
-def test_icu_base_bundle(icu_base_bundle_resources):
-    bundle = Bundle()
-    bundle.add_entry(icu_base_bundle_resources)
-    bundle.request(FHIR_SERVER)
-    assert bundle.response['resourceType'] == 'Bundle'
+def test_icu_base_bundle(db_conn):
+    # Get patient_id that has resources from the resource_list
+    resource_list = [
+        'encounter_icu', 'procedure_icu', 'medication_administration_icu'
+    ]
+    patient_id = get_pat_id_with_links(db_conn, resource_list)
+
+    # Create bundle
+    bundler = Bundler(patient_id)
+
+    # Generate and post patient bundle, must do first to avoid referencing issues
+    bundler.generate_patient_bundle()
+    bundler.patient_bundle.request(FHIR_SERVER)
+
+    #  Generate and post icu base bundle
+    bundler.generate_icu_base_bundle()
+    response = bundler.icu_base_bundle.request(FHIR_SERVER)
+    logging.error(patient_id)
+    assert response['resourceType'] == 'Bundle'
 
 
 # Test all observation resources coming out of the ICU
-def test_icu_observation_bundle(icu_observation_bundle_resources):
-    bundle = Bundle()
-    bundle.add_entry(icu_observation_bundle_resources)
-    bundle.request(FHIR_SERVER)
-    assert bundle.response['resourceType'] == 'Bundle'
+def test_icu_observation_bundle(db_conn):
+    # Get patient_id that has resources from the resource_list
+    resource_list = [
+        'observation_chartevents', 'observation_datetimeevents',
+        'observation_outputevents'
+    ]
+    patient_id = get_pat_id_with_links(db_conn, resource_list)
+
+    # Create bundle
+    bundler = Bundler(patient_id)
+
+    # Generate and post patient bundle, must do first to avoid referencing issues
+    bundler.generate_patient_bundle()
+    bundler.patient_bundle.request(FHIR_SERVER)
+
+    #  Generate and post icu observation bundle
+    bundler.generate_icu_obs_bundle()
+    response = bundler.icu_obs_bundle.request(FHIR_SERVER)
+    logging.error(patient_id)
+    assert response['resourceType'] == 'Bundle'
