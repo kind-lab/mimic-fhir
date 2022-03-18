628bf172b4445cc5b7d4468ea5f88a8b52d73215
alexmbennett2 <alexmbennett2@gmail.com>
2022-03-18 09:31:39 -0400

    Updated admission type icu query

--- sql/codesystem/cs-admission-type-icu.sql
+++ sql/codesystem/cs-admission-type-icu.sql
@@ -1,5 +1,6 @@
 -- Admission type icu CodeSystem
 -- Codes will need to be mapped to match US Core EncounterType - http://hl7.org/fhir/us/core/ValueSet-us-core-encounter-type.html
+-- Source from mimic_core.transfers table, since it holds all careunits that could be used in ICU
 
 
 DROP TABLE IF EXISTS fhir_trm.cs_admission_type_icu;
@@ -8,5 +9,6 @@ CREATE TABLE fhir_trm.cs_admission_type_icu(
 );
 
 INSERT INTO fhir_trm.cs_admission_type_icu
-SELECT DISTINCT first_careunit 
-FROM mimic_icu.icustays
+SELECT DISTINCT careunit 
+FROM mimic_core.transfers
+WHERE careunit IS NOT NULL
