5f358cdc2f380638bf2fa4c0ff8bf7c8d43aee00
alexmbennett2 <alexmbennett2@gmail.com>
2022-06-05 08:55:20 -0400

    py_mimic_fhir setup files

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+readme = open('./pyREADME.md', 'r')
 
 setup(
-    name='py_mimic_fhir',
-    version='0.1.0',
+    name='py-mimic-fhir',
+    version='0.9.2',
     author='Alex Bennett',
-    packages=['py_mimic_fhir'],
-    description='A package to help convert mimic to fhir',
-    install_requires=["pandas"]
+    author_email='alexmbennett2@gmail.com',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    description='A package to help convert MIMIC to FHIR',
+    long_description=readme.read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/kind-lab/mimic-fhir',
+    install_requires=['pandas', 'numpy', 'requests', 'psycopg2>=2.86'],
+    python_requires=">=3.8",
 )
