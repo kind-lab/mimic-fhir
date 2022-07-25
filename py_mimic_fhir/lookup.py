07880e0a39d45c616bc0785afc798d29774a2674
alexmbennett2 <alexmbennett2@gmail.com>
2022-07-25 18:40:50 -0400

    remove unused valuesets/codesystems

--- py_mimic_fhir/lookup.py
+++ py_mimic_fhir/lookup.py
@@ -86,37 +86,33 @@ MIMIC_FHIR_PROFILE_NAMES = [
 ]
 
 MIMIC_CODESYSTEMS = [
-    'admission_class', 'admission_type', 'admit_source', 'bodysite',
-    'chartevents_d_items', 'd_items', 'd_labitems', 'diagnosis_icd9',
-    'diagnosis_icd10', 'discharge_disposition', 'hcpcs_cd', 'identifier_type',
-    'lab_flags', 'lab_fluid', 'lab_priority', 'medadmin_category_icu',
-    'medication_etc', 'medication_icu', 'medication_formulary_drug_cd',
-    'medication_frequency', 'medication_gsn', 'medication_method',
-    'medication_method_icu', 'medication_name', 'medication_ndc',
-    'medication_poe_iv', 'medication_route', 'medication_site',
-    'microbiology_antibiotic', 'microbiology_interpretation',
-    'microbiology_organism', 'microbiology_test', 'observation_category',
-    'procedure_category', 'procedure_icd9', 'procedure_icd10', 'services',
-    'spec_type_desc', 'units'
+    'admit_source', 'bodysite', 'chartevents_d_items', 'd_items', 'd_labitems',
+    'diagnosis_icd9', 'diagnosis_icd10', 'discharge_disposition', 'lab_fluid',
+    'lab_priority', 'medadmin_category_icu', 'medication_etc', 'medication_icu',
+    'medication_formulary_drug_cd', 'medication_frequency', 'medication_gsn',
+    'medication_method', 'medication_method_icu', 'medication_name',
+    'medication_ndc', 'medication_poe_iv', 'medication_route',
+    'medication_site', 'microbiology_antibiotic', 'microbiology_organism',
+    'microbiology_test', 'observation_category', 'procedure_category',
+    'procedure_icd9', 'procedure_icd10', 'services', 'spec_type_desc', 'units'
 ]
 
 MIMIC_VALUESETS = [
-    'admission_class', 'admission_type', 'admit_source', 'bodysite',
-    'chartevents_d_items', 'd_labitems', 'datetimeevents_d_items',
-    'diagnosis_icd', 'discharge_disposition', 'encounter_type',
-    'identifier_type', 'lab_flags', 'lab_priority', 'medadmin_category_icu',
-    'medication', 'medication_etc', 'medication_frequency', 'medication_gsn',
+    'admit_source', 'bodysite', 'chartevents_d_items', 'd_labitems',
+    'datetimeevents_d_items', 'diagnosis_icd', 'discharge_disposition',
+    'encounter_type', 'lab_priority', 'medadmin_category_icu', 'medication',
+    'medication_etc', 'medication_frequency', 'medication_gsn',
     'medication_method', 'medication_method_icu', 'medication_route',
-    'medication_site', 'microbiology_antibiotic', 'microbiology_interpretation',
-    'microbiology_organism', 'microbiology_test', 'observation_category',
-    'outputevents_d_items', 'procedure_category', 'procedureevents_d_items',
-    'procedure_icd', 'services', 'specimen_type', 'units'
+    'medication_site', 'microbiology_antibiotic', 'microbiology_organism',
+    'microbiology_test', 'observation_category', 'outputevents_d_items',
+    'procedure_category', 'procedureevents_d_items', 'procedure_icd',
+    'services', 'specimen_type', 'units'
 ]
 
 VALUESETS_COMPLEX = [
-    'admission_class', 'admission_type', 'datetimeevents_d_items',
-    'diagnosis_icd', 'encounter_type', 'medication', 'outputevents_d_items',
-    'procedureevents_d_items', 'procedure_icd', 'specimen_type'
+    'datetimeevents_d_items', 'diagnosis_icd', 'encounter_type', 'medication',
+    'outputevents_d_items', 'procedureevents_d_items', 'procedure_icd',
+    'specimen_type'
 ]
 # ORDER MATTERS!!
 # The patient bundle must be first and the icu_encounter bundle must be before all other icu bundles
