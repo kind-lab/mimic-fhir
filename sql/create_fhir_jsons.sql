3f329fb9f75c0f2460551de59ff290505b67e321
alexmbennett2 <alexmbennett2@gmail.com>
2022-08-26 16:40:39 -0400

    output ndjsons update to include ED

--- sql/create_fhir_jsons.sql
+++ sql/create_fhir_jsons.sql
@@ -8,81 +8,113 @@
 
 -- institutional resources
 \echo organization
-\o :outputdir/organization.ndjson
+\o :outputdir/MimicOrganization.ndjson
 SELECT fhir FROM mimic_fhir.organization;
+
+\echo location
+\o :outputdir/MimicLocation.ndjson
+SELECT fhir FROM mimic_fhir.location;
 
 -- patient tracking resources
 \echo patient
-\o :outputdir/patient.ndjson
+\o :outputdir/MimicPatient.ndjson
 SELECT fhir FROM mimic_fhir.patient;
 
 \echo encounter
-\o :outputdir/encounter.ndjson
+\o :outputdir/MimicEncounter.ndjson
 SELECT fhir FROM mimic_fhir.encounter;
 
+\echo encounter
+\o :outputdir/MimicEncounterED.ndjson
+SELECT fhir FROM mimic_fhir.encounter_ed;
+
 \echo encounter_icu
-\o :outputdir/encounter_icu.ndjson
+\o :outputdir/MimicEncounterICU.ndjson
 SELECT fhir FROM mimic_fhir.encounter_icu;
 
 -- data resources: conditions, diagnoses, procedures
 \echo condition
-\o :outputdir/condition.ndjson
+\o :outputdir/MimicCondition.ndjson
 SELECT fhir FROM mimic_fhir.condition;
 
+\echo condition ED
+\o :outputdir/MimicConditionED.ndjson
+SELECT fhir FROM mimic_fhir.condition_ed;
+
 \echo procedure
-\o :outputdir/procedure.ndjson
+\o :outputdir/MimicProcedure.ndjson
 SELECT fhir FROM mimic_fhir.procedure;
 
+\echo procedure ED
+\o :outputdir/MimicProcedureED.ndjson
+SELECT fhir FROM mimic_fhir.procedure_ed;
+
 \echo procedure_icu
-\o :outputdir/procedure_icu.ndjson
+\o :outputdir/MimicProcedureICU.ndjson
 SELECT fhir FROM mimic_fhir.procedure_icu;
 
 -- data resources: medications
 \echo medication
-\o :outputdir/medication.ndjson
+\o :outputdir/MimicMedication.ndjson
 SELECT fhir FROM mimic_fhir.medication;
 
 \echo medication_request
-\o :outputdir/medication_request.ndjson
+\o :outputdir/MimicMedicationRequest.ndjson
 SELECT fhir FROM mimic_fhir.medication_request;
 
 \echo medication_dispense
-\o :outputdir/medication_dispense.ndjson
+\o :outputdir/MimicMedicationDispense.ndjson
 SELECT fhir FROM mimic_fhir.medication_dispense;
 
+\echo medication_dispense_ed
+\o :outputdir/MimicMedicationDispenseED.ndjson
+SELECT fhir FROM mimic_fhir.medication_dispense_ed;
+
 \echo medadmin
-\o :outputdir/medadmin.ndjson
+\o :outputdir/MimicMedicationAdministration.ndjson
 SELECT fhir FROM mimic_fhir.medication_administration;
 
 \echo medication_administration_icu
-\o :outputdir/medication_administration_icu.ndjson
+\o :outputdir/MimicMedicationAdministrationICU.ndjson
 SELECT fhir FROM mimic_fhir.medication_administration_icu;
 
+\echo medication_statement_ed
+\o :outputdir/MimicMedicationStatementED.ndjson
+SELECT fhir FROM mimic_fhir.medication_statement_ed;
 
+-- diagnostic resources
 \echo observation_labevents
-\o :outputdir/observation_labevents.ndjson
+\o :outputdir/MimicObservationLabevents.ndjson
 SELECT fhir FROM mimic_fhir.observation_labevents;
 
 \echo observation_micro_org
-\o :outputdir/observation_micro_org.ndjson
+\o :outputdir/MimicObservationMicroOrg.ndjson
 SELECT fhir FROM mimic_fhir.observation_micro_org;
 
 \echo observation_micro_susc
-\o :outputdir/observation_micro_susc.ndjson
+\o :outputdir/MimicObservationMicroSusc.ndjson
 SELECT fhir FROM mimic_fhir.observation_micro_susc;
 
 \echo observation_micro_test
-\o :outputdir/observation_micro_test.ndjson
+\o :outputdir/MimicObservationMicroTest.ndjson
 SELECT fhir FROM mimic_fhir.observation_micro_test;
 
 \echo observation_chartevents
-\o :outputdir/observation_chartevents.ndjson
+\o :outputdir/MimicObservationChartevents.ndjson
 SELECT fhir FROM mimic_fhir.observation_chartevents;
 
 \echo observation_datetimeevents
-\o :outputdir/observation_datetimeevents.ndjson
+\o :outputdir/MimicObservationDatetimeevents.ndjson
 SELECT fhir FROM mimic_fhir.observation_datetimeevents;
 
 \echo observation_outputevents
-\o :outputdir/observation_outputevents.ndjson
+\o :outputdir/MimicObservationOutputevents.ndjson
 SELECT fhir FROM mimic_fhir.observation_outputevents;
+
+\echo observation_ed
+\o :outputdir/MimicObservationED.ndjson
+SELECT fhir FROM mimic_fhir.observation_ed;
+
+\echo observation_vitalsigns
+\o :outputdir/MimicObservationVitalSigns.ndjson
+SELECT fhir FROM mimic_fhir.observation_vital_signs;
