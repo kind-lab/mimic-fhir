834da2f5b972c87b500fa92efbecce1af57a7f24
alexmbennett2 <alexmbennett2@gmail.com>
2022-09-08 11:22:42 -0400

    location array for physicalType dropped

--- sql/fhir_location.sql
+++ sql/fhir_location.sql
@@ -32,13 +32,13 @@ SELECT
             )
         ) 
         , 'name', tfr_CAREUNIT
-        , 'physicalType', jsonb_build_array(jsonb_build_object(
+        , 'physicalType', jsonb_build_object(
             'coding', jsonb_build_array(json_build_object(
                 'system', 'http://terminology.hl7.org/CodeSystem/location-physical-type'
                 , 'code', 'wa'
                 , 'display', 'Ward'
             ))
-        ))
+        )
         , 'status', 'active'
         , 'managingOrganization', jsonb_build_object('reference', 'Organization/' || bidmc_UUID)
     )) as fhir
