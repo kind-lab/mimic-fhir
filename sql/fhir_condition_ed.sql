aec446c4ff1249bbe45b7aac536693d797ac5fa8
Piotr Szul <piotr.szul@csiro.au>
2024-06-10 16:05:55 +1000

    Aligning coding display names in fhir resources with the display names in the code system for cs-diagnosis-icd9, cs-diagnosis-icd10 and cs-medication-etc to fix:
    https://github.com/kind-lab/mimic-fhir/issues/113
    https://github.com/kind-lab/mimic-fhir/issues/114
    
    'Wrong Display Name  XXX' errors, e.g:
    Wrong Display Name 'Acne Therapy Systemic - Tetracycline antibiotic' for http://mimic.mit.edu/fhir/mimic/CodeSystem/mimic-medication-etc#00005953. Valid display is 'Acne Therapy Systemic - Tetracyclines' (en) (for the language(s) '--')

--- sql/fhir_condition_ed.sql
+++ sql/fhir_condition_ed.sql
@@ -5,15 +5,19 @@ SELECT fhir_etl.fn_create_table_patient_dependent('condition_ed');
 
 WITH fhir_condition_ed AS (
     SELECT
-        TRIM(diag.icd_code) AS diag_ICD_CODE
-        , diag.icd_title  AS diag_ICD_TITLE
+        CASE WHEN diag.icd_version = 9
+            THEN cs_diagnosis_icd9.code
+            ELSE cs_diagnosis_icd10.code
+        END AS diag_ICD_CODE
+        , CASE WHEN diag.icd_version = 9
+            THEN cs_diagnosis_icd9.display
+            ELSE cs_diagnosis_icd10.display
+        END AS diag_ICD_DISPLAY
         , diag.icd_version AS diag_ICD_VERSION
         , CASE WHEN diag.icd_version = 9 
             THEN 'http://mimic.mit.edu/fhir/mimic/CodeSystem/mimic-diagnosis-icd9' 
             ELSE 'http://mimic.mit.edu/fhir/mimic/CodeSystem/mimic-diagnosis-icd10'
         END AS diag_ICD_SYSTEM
-            
-  
         -- reference uuids
         , uuid_generate_v5(ns_condition.uuid, diag.stay_id || '-' || diag.seq_num || '-' || diag.icd_code) as uuid_DIAGNOSIS
         , uuid_generate_v5(ns_patient.uuid, CAST(diag.subject_id AS TEXT)) as uuid_SUBJECT_ID
@@ -22,6 +26,18 @@ WITH fhir_condition_ed AS (
         mimiciv_ed.diagnosis diag
         INNER JOIN mimiciv_hosp.patients pat
             ON diag.subject_id = pat.subject_id
+
+        -- code systems
+        LEFT OUTER JOIN fhir_trm.cs_diagnosis_icd9 AS cs_diagnosis_icd9
+            ON TRIM(diag.icd_code) = cs_diagnosis_icd9.code
+            AND diag.icd_version = 9
+
+        LEFT OUTER JOIN fhir_trm.cs_diagnosis_icd10 AS cs_diagnosis_icd10
+            ON TRIM(diag.icd_code) = cs_diagnosis_icd10.code
+            AND diag.icd_version = 10
+
+        -- uuid namespaces
+
         LEFT JOIN fhir_etl.uuid_namespace ns_encounter 
             ON ns_encounter.name = 'EncounterED'
         LEFT JOIN fhir_etl.uuid_namespace ns_patient 
@@ -53,8 +69,8 @@ SELECT
         , 'code', jsonb_build_object(
             'coding', jsonb_build_array(jsonb_build_object(
                 'system', diag_ICD_SYSTEM
-                , 'code', diag_ICD_CODE
-                , 'display', diag_ICD_TITLE
+                , 'code', TRIM(diag_ICD_CODE)
+                , 'display', TRIM(diag_ICD_DISPLAY)
             ))
         )
         , 'subject', jsonb_build_object('reference', 'Patient/' || uuid_SUBJECT_ID)
