b2beb9e8c4fb2b41147626e18bb4c1867f328ee3
Piotr Szul <piotr.szul@csiro.au>
2024-06-10 12:28:11 +1000

    Removing leading ' ' from display name ' Black ...'  in map_race_omb.sql to fix:
    https://github.com/kind-lab/mimic-fhir/issues/95
    
    The Coding provided (urn:oid:2.16.840.1.113883.6.238#2054-5) was not found in the value set 'OMB Race Categories' (http://hl7.org/fhir/us/core/ValueSet/omb-race-category|4.0.0), and a code is required from this value set. (error message = Wrong whitespace in Display Name ' Black or African American' for urn:oid:2.16.840.1.113883.6.238#2054-5. Valid display is 'Black or African American' (for the language(s) '--'))

--- sql/fhir_etl/map_race_omb.sql
+++ sql/fhir_etl/map_race_omb.sql
@@ -10,7 +10,7 @@ CREATE TABLE fhir_etl.map_race_omb(
 INSERT INTO fhir_etl.map_race_omb
     (mimic_race, fhir_race_omb_display, fhir_race_omb_code, fhir_system)
 VALUES 
-    ('BLACK/AFRICAN', ' Black or African American', '2054-5', 'urn:oid:2.16.840.1.113883.6.238'),
+    ('BLACK/AFRICAN', 'Black or African American', '2054-5', 'urn:oid:2.16.840.1.113883.6.238'),
     ('MULTIPLE RACE/ETHNICITY', 'other', 'OTH', 'http://terminology.hl7.org/CodeSystem/v3-NullFlavor'),
     ('WHITE - OTHER EUROPEAN', 'White', '2106-3', 'urn:oid:2.16.840.1.113883.6.238'),
     ('WHITE - EASTERN EUROPEAN', 'White', '2106-3', 'urn:oid:2.16.840.1.113883.6.238'),
@@ -18,14 +18,14 @@ VALUES
     ('ASIAN - ASIAN INDIAN', 'Asian', '2028-9', 'urn:oid:2.16.840.1.113883.6.238'),
     ('ASIAN - KOREAN', 'Asian', '2028-9', 'urn:oid:2.16.840.1.113883.6.238'),
     ('ASIAN - CHINESE', 'Asian', '2028-9', 'urn:oid:2.16.840.1.113883.6.238'),
-    ('BLACK/AFRICAN AMERICAN', ' Black or African American', '2054-5', 'urn:oid:2.16.840.1.113883.6.238'),
+    ('BLACK/AFRICAN AMERICAN', 'Black or African American', '2054-5', 'urn:oid:2.16.840.1.113883.6.238'),
     ('AMERICAN INDIAN/ALASKA NATIVE', 'American Indian or Alaska Native', '1002-5', 'urn:oid:2.16.840.1.113883.6.238'),
     ('NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER', 'Native Hawaiian or Other Pacific Islander', '2076-8', 'urn:oid:2.16.840.1.113883.6.238'),
-    ('BLACK/CAPE VERDEAN', ' Black or African American', '2054-5', 'urn:oid:2.16.840.1.113883.6.238'),
+    ('BLACK/CAPE VERDEAN', 'Black or African American', '2054-5', 'urn:oid:2.16.840.1.113883.6.238'),
     ('WHITE', 'White', '2106-3', 'urn:oid:2.16.840.1.113883.6.238'),
     ('WHITE - BRAZILIAN', 'White', '2106-3', 'urn:oid:2.16.840.1.113883.6.238'),
     ('WHITE - RUSSIAN', 'White', '2106-3', 'urn:oid:2.16.840.1.113883.6.238'),
-    ('BLACK/CARIBBEAN ISLAND', ' Black or African American', '2054-5', 'urn:oid:2.16.840.1.113883.6.238'),
+    ('BLACK/CARIBBEAN ISLAND', 'Black or African American', '2054-5', 'urn:oid:2.16.840.1.113883.6.238'),
     ('ASIAN - SOUTH EAST ASIAN', 'Asian', '2028-9', 'urn:oid:2.16.840.1.113883.6.238'),
     ('PORTUGUESE', 'White', '2106-3', 'urn:oid:2.16.840.1.113883.6.238'),
     ('HISPANIC/LATINO - CUBAN', 'White', '2106-3', 'urn:oid:2.16.840.1.113883.6.238'),
