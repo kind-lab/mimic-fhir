5f6c9953c63eca9fa97567393809a895e77b116e
Piotr Szul <piotr.szul@csiro.au>
2024-06-17 12:23:26 +1000

    Producing NULL for blank dose_given_unit in fhir_medication_administration.sql to fix:
    https://github.com/kind-lab/mimic-fhir/issues/109
    
    Invalid attribute value "": Attribute value must not be empty ("")
    (e.g: {"dose": {"code": "", "unit": "", "value": 1,)

--- sql/fhir_medication_administration.sql
+++ sql/fhir_medication_administration.sql
@@ -58,7 +58,8 @@ WITH prescriptions AS (
             WHEN emd.dose_given IN ('N', 'INI') THEN 
                 TRIM(emd.product_unit)
             WHEN emd.dose_given ~ '^[0-9\.]+$' THEN -- ALL NUMERIC
-                TRIM(emd.dose_given_unit)
+                -- NULLIF to treat blanks strings as NULLs
+                NULLIF(TRIM(emd.dose_given_unit), '')
             ELSE NULL -- accounts FOR 46,000 VALUES WITH FREE text      
         END AS emd_DOSE_GIVEN_UNIT
         
